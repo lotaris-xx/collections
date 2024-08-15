1cc933cbd2adfd1db42299285f9e3962e350b672
Allen Smith <allsmith@redhat.com>
2024-08-15 11:28:15 -0400

    Move common functions

---
+++ maas_settings/plugins/module_utils/maas_common.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+
+class maas_api_cred:
+    """
+    Represents a MAAS API Credenital
+    Provides both MAAS API and OAuth terminology
+    """
+
+    def __init__(self, api_json):
+        self.consumer_key = api_json["consumer_key"]
+        self.token_key = api_json["token_key"]
+        self.token_secret = api_json["token_secret"]
+
+        self.client_key = self.consumer_key
+        self.resource_owner_key = self.token_key
+        self.resource_owner_secret = self.token_secret
+
+
+def grab_maas_apikey(module):
+    """
+    Connect to MAAS API and grab the 3 part API key
+    """
+    consumer = "ansible@host"
+    uri = "/accounts/authenticate/"
+    site = module.params["site"]
+    username = module.params["username"]
+    password = module.params["password"]
+
+    payload = {
+        "username": username,
+        "password": password,
+        "consumer": consumer,
+    }
+    try:
+        r = post(site + uri, data=payload)
+        r.raise_for_status()
+        return r
+    except exceptions.RequestException as e:
+        module.fail_json(msg="Auth failed: {}".format(str(e)))
