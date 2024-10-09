f37c3ee4239dc06842e1d0a1e0320d328be64d2f
Allen Smith <allsmith@redhat.com>
2024-10-09 14:09:23 -0400

    Handle case where we already have an API token

--- maas_settings/plugins/module_utils/maas_common.py
+++ maas_settings/plugins/module_utils/maas_common.py
@@ -22,25 +22,47 @@ class maas_api_cred:
         self.resource_owner_key = self.token_key
         self.resource_owner_secret = self.token_secret
 
+    def __repr__(self):
+        return f"client_key: {self.client_key}, resource_owner_key: {self.resource_owner_key}, resource_owner_secret: {self.resource_owner_secret}"
+
+    def json(self):
+        # requests has a json() method, simplify the module code
+        return {
+            "consumer_key": self.consumer_key,
+            "token_key": self.token_key,
+            "token_secret": self.token_secret,
+        }
+
 
 def grab_maas_apikey(module):
     """
     Connect to MAAS API and grab the 3 part API key
     """
-    consumer = "ansible@host"
-    uri = "/accounts/authenticate/"
-    site = module.params["site"]
-    username = module.params["username"]
-    password = module.params["password"]
+    if module.params["token"]:
+        # We have a token, split it and return the parts
+        (consumer_key, token_key, token_secret) = module.params["token"].split(":")
+        return maas_api_cred(
+            {
+                "consumer_key": consumer_key,
+                "token_key": token_key,
+                "token_secret": token_secret,
+            }
+        )
+    else:
+        consumer = "ansible@host"
+        uri = "/accounts/authenticate/"
+        site = module.params["site"]
+        username = module.params["username"]
+        password = module.params["password"]
 
-    payload = {
-        "username": username,
-        "password": password,
-        "consumer": consumer,
-    }
-    try:
-        r = post(site + uri, data=payload)
-        r.raise_for_status()
-        return r
-    except exceptions.RequestException as e:
-        module.fail_json(msg="Auth failed: {}".format(str(e)))
+        payload = {
+            "username": username,
+            "password": password,
+            "consumer": consumer,
+        }
+        try:
+            r = post(site + uri, data=payload)
+            r.raise_for_status()
+            return r
+        except exceptions.RequestException as e:
+            module.fail_json(msg="Auth failed: {}".format(str(e)))
